79ae23f637343e82206d754d04c5ff04ee813bee
alex <alex@staticlibs.net>
2016-06-01 13:12:33 +0100

    apidocs added

--- include/staticlib/orm/Connection.hpp
+++ include/staticlib/orm/Connection.hpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016, alex at staticlibs.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /* 
  * File:   Connection.hpp
  * Author: alex
@@ -10,6 +26,7 @@
 
 #include <string>
 #include <memory>
+#include <vector>
 
 #include "staticlib/pimpl.hpp"
 #include "staticlib/serialization/JsonValue.hpp"
@@ -20,6 +37,10 @@
 namespace staticlib {
 namespace orm {
 
+/**
+ * This class manages database connection. Connection lifetime is bound to RAII,
+ * it will be closed on instance destruction. Connections are NOT thread-safe.
+ */
 class Connection : public staticlib::pimpl::PimplObject {
 protected:
     /**
@@ -34,13 +55,43 @@ public:
      */
     PIMPL_CONSTRUCTOR(Connection)
     
+    /**
+     * Constructor, takes DB URL in SOCI format, see examples: https://github.com/staticlibs/external_soci/blob/160b1ff6d7012b7b9969ae7f4bed655bb6087ff0/test/soci_test.cpp#L44
+     * 
+     * @param url DB connection URL in SOCI format
+     */
     Connection(std::string url);
 
+    /**
+     * Starts a transaction bound to this connection. Transactions are optional
+     * for queries execution, their exact behaviour ("auto-commit" mode etc)
+     * depends on a database.
+     * 
+     * @return transaction bound to this connection
+     */
     Transaction start_transaction();
 
-    std::vector<staticlib::serialization::JsonValue> query(std::string sql, const staticlib::serialization::JsonValue& param =
-            staticlib::serialization::JsonValue{});
+    /**
+     * Executes a "SELECT" query to the database
+     * 
+     * @param sql SQL query text
+     * @param param parameters object, it is treated as a "param_name -> param_value" mapping 
+     *              if that object has a type "OBJECT". Otherwise it is treated as a "param_value".
+     *              "NULL_T" params object is ignored.
+     * @return list of query results as a vector of "JsonValue" objects - one object 
+     *         for each returned row
+     */
+    std::vector<staticlib::serialization::JsonValue> query(std::string sql, 
+            const staticlib::serialization::JsonValue& param = staticlib::serialization::JsonValue{});
 
+    /**
+     * Executes a DML or DDL query to the database
+     * 
+     * @param sql SQL query text
+     * @param param parameters object, it is treated as a "param_name -> param_value" mapping 
+     *              if that object has a type "OBJECT". Otherwise it is treated as a "param_value".
+     *              "NULL_T" params object is ignored.
+     */
     void execute(std::string sql, const staticlib::serialization::JsonValue& param =
             staticlib::serialization::JsonValue{});
 
--- include/staticlib/orm/Transaction.hpp
+++ include/staticlib/orm/Transaction.hpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016, alex at staticlibs.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /* 
  * File:   Transaction.hpp
  * Author: alex
@@ -18,8 +34,13 @@
 namespace staticlib {
 namespace orm {
 
-class Connection;
-
+/**
+ * This class manages a database transaction. Transactions are optional
+ * for queries execution, their exact behaviour ("auto-commit" mode etc)
+ * depends on a database. Transactions lifetime is bound to RAII,
+ * it must be committed explicitely, otherwise it will be rolled-back 
+ * on instance destruction. Transactions are NOT thread-safe.
+ */
 class Transaction : public staticlib::pimpl::PimplObject {
 protected:
     /**
@@ -35,9 +56,16 @@ public:
      */
     PIMPL_CONSTRUCTOR(Transaction)
     
+    /**
+     * Commits this transaction in the database
+     */       
     void commit();
     
-// private api
+    /**
+     * Implementation-internal constructor, not a part of a public API
+     * 
+     * @param session internal
+     */
     Transaction(void* /* soci::session* */ session);
     
     
